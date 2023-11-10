f9df8e8d62be0b5a52bd7db7c078b4a3a5bc7798
Diana Rios <31250962+dianario@users.noreply.github.com>
2023-11-10 12:07:30 +0000

    Load daily data and check redundancy

--- daily_activity_trends.R
+++ daily_activity_trends.R
@@ -4,25 +4,49 @@ library(tidyverse)
 library(skimr)
 library(janitor)
 daily_activity <- read_csv("./fitabase_data/dailyActivity_merged.csv")
+daily_calories <- read_csv("./fitabase_data/dailyCalories_merged.csv")
+daily_intensities <- read_csv("./fitabase_data/dailyIntensities_merged.csv")
+daily_steps <- read_csv("./fitabase_data/dailySteps_merged.csv")
 
 #explore data
-View(daily_activity)
-head(daily_activity)
 str(daily_activity)
+str(daily_calories)
+str(daily_intensities)
+str(daily_steps)
 
-#Coerce Id col as character
+# The daily activity data frame contains columns which appear in the other daily data frames. 
+# This is confirmed by checking if the data in those columns is indeed identical. All the daily data
+# is contained within the daily_activity data frame. In the case of daily_steps, 
+# the only mismatch is located in column names, but all values are equal. 
+
+daily_activity %>% select(Calories) %>% all.equal(daily_calories %>% select(Calories))
+daily_activity %>% select(VeryActiveDistance, 
+                          LightActiveDistance, 
+                          SedentaryActiveDistance) %>% 
+  all.equal(daily_intensities %>% 
+              select(VeryActiveDistance, LightActiveDistance, SedentaryActiveDistance))
+daily_activity %>%  select(VeryActiveMinutes, LightlyActiveMinutes, SedentaryMinutes) %>% 
+  all.equal(daily_intensities %>%  select(VeryActiveMinutes, LightlyActiveMinutes, SedentaryMinutes))
+daily_activity %>% select(TotalSteps) %>% all.equal(daily_steps %>% select(StepTotal))
+
+
+# Begin working with the daily_activity data frame.
+##
+##
+##
+
+# First change variable types. 
+# Coerce Id col as character
 daily_activity <- daily_activity %>% 
   mutate(Id = as.character(Id)) 
 str(daily_activity)
 
-# coerce ActivityDate as date
+# coerce ActivityDate col as date
 daily_activity <- daily_activity %>% 
   mutate(ActivityDate = mdy(ActivityDate))
 str(daily_activity)
 
-skim_without_charts(daily_activity)
-
-#Data validation
+# Data validation
 
 #Find out number of unique ID values 
 unique_id_count <- daily_activity %>% 
@@ -30,19 +54,21 @@ unique_id_count <- daily_activity %>%
   summarise(count = n())
 print(unique_id_count)
 
-# The nunmber of unique IDs exceeded the number of users reported, so this is 
-# investigated futher. 
+# The number of unique IDs exceeded the number of users reported in data description, so this was
+# investigated further. All the IDs are different, with no apparent typos. Therefore is is unlikely
+# there are any mistakes. 
 
 print(daily_activity %>% distinct(Id), n=33)
 
-# this has to be investigated further in other tables to see if this is real or a problem
 
-# check date range 
+
+# check date range. The date range is correct
 date_range <-daily_activity %>% 
   summarise(StartDate = min(ActivityDate, na.rm = TRUE),
 EndDate = max(ActivityDate, na.rm = TRUE))
 print(date_range)
-#date range is correct
+
+
 
 #check the active minute data adds up
 daily_activity <- daily_activity %>% 
@@ -59,6 +85,33 @@ daily_activity <- daily_activity %>%
   mutate(TotalActiveMinutes = VeryActiveMinutes + FairlyActiveMinutes + LightlyActiveMinutes)
 str(daily_activity)
 
-ggplot(data = daily_activity) + geom_point(mapping=aes(x=ActivityDate, y=TotalActiveMinutes)) 
+ggplot(data = daily_activity) + 
+  geom_line(mapping=aes(x=ActivityDate, y=TotalActiveMinutes, color=Id)) +
+  labs(title = "Time spent active per user", 
+  x = "Date", y ="Total Active Minutes")
+
+
+average_active_time <- daily_activity %>%
+  group_by(ActivityDate) %>%
+  summarise(AverageActivity = mean(TotalActiveMinutes, na.rm = TRUE)) 
+
+ggplot(data = average_active_time, aes(x = ActivityDate, y = AverageActivity)) +
+  geom_line() + # Use geom_line() for line plot
+  geom_point() + # Use geom_point() to show the actual data points
+  theme_minimal() +
+  labs(
+    title = "",
+    x = "Date",
+    y = "Total"
+  )
  
+ggplot(data = daily_activity, aes(x = ActivityDate)) +
+  geom_jitter(aes(y = TotalActiveMinutes), width = 0.1, height = 0, color = "black", alpha = 0.5) +
+  geom_col(data = average_active_time, aes(y = AverageActivity), fill = "blue", alpha = 0.5) +
+  theme_minimal() +
+  labs(
+    title = "Daily Activity and Average Active Time",
+    x = "Date",
+    y = "Total Active Minutes"
+  )
   
